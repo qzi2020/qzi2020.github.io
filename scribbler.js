41a7d6525c75b13d02926de0857121549e3067be
qzi2020 <77766964+qzi2020@users.noreply.github.com>
2021-01-21 18:03:32 +0800

    Initial commit

---
+++ scribbler.js
@@ -0,0 +1,149 @@
+// utilities
+var get = function (selector, scope) {
+  scope = scope ? scope : document;
+  return scope.querySelector(selector);
+};
+
+var getAll = function (selector, scope) {
+  scope = scope ? scope : document;
+  return scope.querySelectorAll(selector);
+};
+
+// setup typewriter effect in the terminal demo
+if (document.getElementsByClassName('demo').length > 0) {
+  var i = 0;
+  var txt = `
+  
+            -智能制造自动化-  Robotic(RobotStudio...), Microcontroller (Proteus...), PLC (GX Works2, CoDeSys...), Emulation (Simumatik3D...) Anyone, Anytime, Anywhere, Anythink, Anyhow, 5A 
+
+            -区块链技术融合-  ETH乙太币、ERC-20权证、SmartContract智能合约、DAPP去中心化应用
+
+            -电子设计自动化-  电路设计、性能分析到IC设计图或PCB设计图整个过程在计算机上自动处理完成。
+			
+            -智能算法-  人工神经网络（Artificial Neural Network）深度学习使机器模仿视听和思考等人类的活动，解决很多复杂的模式识别难题。
+            
+			-跨学科整合-  光学技术、机械技术、材料技术、电气及微电子技术、计算机技术、控制技术、信息通信技术的相互交叉与融合。
+			
+            -智能金融自动化-  程序自动化交易从自动化交易的角度借助在回测时获取的大量市场数据与正确的投资策略。 🥝`;
+  var speed = 60;
+
+  function typeItOut () {
+    if (i < txt.length) {
+      document.getElementsByClassName('demo')[0].innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeItOut, speed);
+    }
+  }
+
+  setTimeout(typeItOut, 1800);
+}
+
+// toggle tabs on codeblock
+window.addEventListener("load", function() {
+  // get all tab_containers in the document
+  var tabContainers = getAll(".tab__container");
+
+  // bind click event to each tab container
+  for (var i = 0; i < tabContainers.length; i++) {
+    get('.tab__menu', tabContainers[i]).addEventListener("click", tabClick);
+  }
+
+  // each click event is scoped to the tab_container
+  function tabClick (event) {
+    var scope = event.currentTarget.parentNode;
+    var clickedTab = event.target;
+    var tabs = getAll('.tab', scope);
+    var panes = getAll('.tab__pane', scope);
+    var activePane = get(`.${clickedTab.getAttribute('data-tab')}`, scope);
+
+    // remove all active tab classes
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].classList.remove('active');
+    }
+
+    // remove all active pane classes
+    for (var i = 0; i < panes.length; i++) {
+      panes[i].classList.remove('active');
+    }
+
+    // apply active classes on desired tab and pane
+    clickedTab.classList.add('active');
+    activePane.classList.add('active');
+  }
+});
+
+//in page scrolling for documentaiton page
+var btns = getAll('.js-btn');
+var sections = getAll('.js-section');
+
+function setActiveLink(event) {
+  // remove all active tab classes
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].classList.remove('selected');
+  }
+
+  event.target.classList.add('selected');
+}
+
+function smoothScrollTo(element, event) {
+  setActiveLink(event);
+
+  window.scrollTo({
+    'behavior': 'smooth',
+    'top': element.offsetTop - 20,
+    'left': 0
+  });
+}
+
+if (btns.length && sections.length > 0) {
+// for (var i = 0; i<btns.length; i++) {
+//   btns[i].addEventListener('click', function(event) {
+//     smoothScrollTo(sections[i], event);
+//   });
+// }
+  btns[0].addEventListener('click', function (event) {
+    smoothScrollTo(sections[0], event);
+  });
+
+  btns[1].addEventListener('click', function (event) {
+    smoothScrollTo(sections[1], event);
+  });
+
+  btns[2].addEventListener('click', function (event) {
+    smoothScrollTo(sections[2], event);
+  });
+
+  btns[3].addEventListener('click', function (event) {
+    smoothScrollTo(sections[3], event);
+  });
+}
+
+// fix menu to page-top once user starts scrolling
+window.addEventListener('scroll', function () {
+  var docNav = get('.doc__nav > ul');
+
+  if( docNav) {
+    if (window.pageYOffset > 63) {
+      docNav.classList.add('fixed');
+    } else {
+      docNav.classList.remove('fixed');
+    }
+  }
+});
+
+// responsive navigation
+var topNav = get('.menu');
+var icon = get('.toggle');
+
+window.addEventListener('load', function(){
+  function showNav() {
+    if (topNav.className === 'menu') {
+      topNav.className += ' responsive';
+      icon.className += ' open';
+    } else {
+      topNav.className = 'menu';
+      icon.classList.remove('open');
+    }
+  }
+  icon.addEventListener('click', showNav);
+});
